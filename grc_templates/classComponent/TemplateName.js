7c5dd9cb4f6987c7858e0020617c99cb206bf117
alex <desorbaix@free.fr>
2021-08-24 09:14:38 +0200

    configuration d'un modele class de generation de composant

---
+++ grc_templates/classComponent/TemplateName.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './TemplateName.module.css'
+import PropTypes from 'prop-types'
+
+class TemplateName extends React.Component{
+    constructor(props)
+    {
+        super(props);
+    }
+    render(){
+        return <div className={styles.TemplateName}>templateName</div>
+    }
+}
+TemplateName.propTypes={
+
+}
+TemplateName.defaultProps={
+
+}
+export default TemplateName;
